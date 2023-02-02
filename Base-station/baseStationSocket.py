b5f112b6c0e2c041410f8080889345ba80b3ba24
Mattias Petersson <ma8884pe-s@student.lu.se>
2023-02-02 16:25:11 +0100

    Init sockets, work locally now.
    UDP had the problem that you can't listen on a UDP port. Will fix when we are actually sending data over the wireless NRF24L01 modules.

--- Base-station/baseStationSocket.py
+++ Base-station/baseStationSocket.py
@@ -4,15 +4,16 @@ import socket
 recieverPort = 9003 #Out of common port numbers on the Wiki, anything above 1024 is free to use and not reserved. 9003 is ours now.
 
 # Create a socket using the port number, bind the local hostname and the arbitrary port to it:
-localSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #UDP is SOCK_DRAM, TCP is SOCK_STREAM
+localSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #UDP is SOCK_DRAM, TCP is SOCK_STREAM
 
-localSocket.bind(socket.gethostname, recieverPort)
+localSocket.bind(('', recieverPort))
 
 localSocket.listen()
 conn, addr = localSocket.accept()
 while (True):
     try:
         data = conn.recv(1024)
+        print(data)
         if not data:
             break
 
