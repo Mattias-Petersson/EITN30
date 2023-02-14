dd42637587e89475ac8100096a8c03fc180af018
Mattias Petersson <ma8884pe-s@student.lu.se>
2023-02-14 11:50:23 +0100

    Tried fixing radio hardware not responding, in original and new file
    New file attempts to use a different library, however similar problems occurred. I also tried to manually set the SPI.

--- nrfnewLibrary.py
+++ nrfnewLibrary.py
@@ -9,25 +9,14 @@ from pytun import TunTapDevice
 import scapy.all as scape
 import queue
 import argparse
-from circuitpython_nrf24l01.rf24 import RF24
+from RF24 import RF24
 
+txRadio = RF24(17, 0)
+rxRadio = RF24(27, 60)
 
-SPI0 = {
-    'SPI': 0,
-    'MOSI':10,#dio.DigitalInOut(board.D10),
-    'MISO':9,#dio.DigitalInOut(board.D9),
-    'clock':11,#dio.DigitalInOut(board.D11),
-    'ce_pin':dio.DigitalInOut(board.D17),
-    'csn':dio.DigitalInOut(board.D8),
-    }
-SPI1 = {
-    'SPI': 10,
-    'MOSI':dio.DigitalInOut(board.D20), #20, D10
-    'MISO':dio.DigitalInOut(board.D19), #19, D9
-    'clock':dio.DigitalInOut(board.D21), #21, D11
-    'ce_pin':dio.DigitalInOut(board.D27),
-    'csn':dio.DigitalInOut(board.D18),
-    }
+# addresses needs to be in a buffer protocol object (bytearray)
+address = [b"1Node", b"2Node"]
+
 
 def fragment(data, fragmentSize):
     """ Fragments and returns a list of any IP packet. The input parameter has to be an IP packet, as this is done via Scapy. (for now) 
@@ -42,10 +31,9 @@ def defrag(dataList):
     return data
  
 #processargs: kwargs={'nrf':tx_nrf, 'address':bytes(args.dst, 'utf-8'), 'queue': incoming, 'channel': args.txchannel, 'size':args.size})
-def tx(nrf: RF24, address, queue: queue, channel, size):
-    nrf.open_tx_pipe(address)
-    nrf.listen = False
-    nrf.channel = channel
+def tx(nrf, address, queue: queue, channel, size):
+    nrf.stopListening()
+
 
 
     print("Init TX")
@@ -54,21 +42,19 @@ def tx(nrf: RF24, address, queue: queue, channel, size):
 
         frags = fragment(packet, size)
         for f in frags:
-            nrf.send(f)
+            nrf.write(f)
 
         print("Do we get here? and if so, how often do we get here?")
 
 #processargs: kwargs={'nrf':rx_nrf, 'address':bytes(args.src, 'utf-8'), 'tun': tun, 'channel': args.rxchannel})
 def rx(nrf, address, tun: TunTapDevice, channel):
     incoming = []
-    nrf.open_rx_pipe(1, address) # Use pipe 1.
-    nrf.listen = True
-    nrf.channel = channel
+    nrf.startListening()
     
     print("Init RX")
     while True:
-        if nrf.available():
-            size = nrf.any()
+        if nrf.available_pipe():
+            size = nrf.getDynamicPayloadSize()
             incoming.append(nrf.read(size))
 
             print(incoming)
@@ -76,25 +62,23 @@ def rx(nrf, address, tun: TunTapDevice, channel):
         tun.write(finished)
 
 
-def setupNRFModules(rx, tx):
-    
-    
-    # From the API, 1 sets freq to 1Mbps, 2 sets freq to 2Mbps, 250 to 250kbps.
-    rx.data_rate = 2 
-    tx.data_rate = 2
-    # Look into what channels are the least populated. 
-   # rx.channel = ??
-   # tx.channel = ??
-   
-    rx.ack = True
-    tx.ack = True
+def setupNRFModules(tx, rx, args):
+    tx.begin()
+    rx.begin()
 
-    rx.payload_length = 32
-    tx.payload_length = 32
+    tx.setChannel(args.txchannel)
+    rx.setChannel(args.rxchannel)
 
-    # From the API, 1 enables CRC using 1 byte (weak), 2 enables CRC using 2 bytes (stronger), 0 disables. 
-    rx.crc = 1
-    tx.crc = 1
+    tx.setDataRate(1)
+    rx.setDataRate(1)
+
+    tx.setCRCLength(1)
+    rx.setCRCLength(1)
+
+    tx.openWritingPipe(address[0])
+    rx.openWritingPipe(1, address[1])
+
+
     
 def setupIP(isBase):
     ipBase = '20.0.0.1'
@@ -125,14 +109,7 @@ def main():
         print("Do note that having tx and rx channels this close to each other can introduce cross-talk.")
 
 
-    SPI0['spi'] = busio.SPI(**{x: SPI0[x] for x in ['clock', 'MOSI', 'MISO']})
-    SPI1['spi'] = busio.SPI(**{x: SPI1[x] for x in ['clock', 'MOSI', 'MISO']})
-
-    # initialize the nRF24L01 on the spi bus object
-  
-    rx_nrf = RF24(SPI0['spi'], SPI0['csn'], SPI0['ce_pin'])
-    tx_nrf = RF24(SPI1['spi'], SPI1['csn'], SPI1['ce_pin'])
-    setupNRFModules(rx_nrf, tx_nrf)
+    setupNRFModules(txRadio, rxRadio, args)
 
     #These might not be needed, but they seem useful considering their get() blocks until data is available.
     outgoing = queue.Queue()
@@ -141,11 +118,11 @@ def main():
 
 
 
-    rx_process = Process(target=rx, kwargs={'nrf':rx_nrf, 'address':bytes(args.src, 'utf-8'), 'tun': tun, 'channel': args.rxchannel})
+    rx_process = Process(target=rx, kwargs={'nrf':rxRadio, 'address':bytes(args.src, 'utf-8'), 'tun': tun, 'channel': args.rxchannel})
     rx_process.start()
     time.sleep(1)
 
-    tx_process = Process(target=tx, kwargs={'nrf':tx_nrf, 'address':bytes(args.dst, 'utf-8'), 'queue': outgoing, 'channel': args.txchannel, 'size':args.size})
+    tx_process = Process(target=tx, kwargs={'nrf':txRadio, 'address':bytes(args.dst, 'utf-8'), 'queue': outgoing, 'channel': args.txchannel, 'size':args.size})
     tx_process.start()
 
     ICMPPacket = scape.IP(dst="8.8.8.8")/scape.ICMP() # Merely for testing. Remove later. 
